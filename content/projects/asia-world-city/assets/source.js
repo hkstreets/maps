6f476380e9268dfc3fd19409ed4eec29d4608e3a
Diana Pang <dianapanghoting@gmail.com>
2023-07-03 00:32:50 +0800

    Add sources for latest release

--- content/projects/asia-world-city/assets/source.js
+++ content/projects/asia-world-city/assets/source.js
@@ -2,233 +2,125 @@ mapboxgl.accessToken =
     "pk.eyJ1IjoiZHBhbmczMTEiLCJhIjoiY2tidmNld2cyMDA5djJwbXYzZjZsNTB0OSJ9.hvW536UZKn2wDDH6yDjJ2g";
 
 const bounds = [
-    [113.755, 22.13], // Southwest coordinates
+    [113.8, 22.13], // Southwest coordinates
     [114.5, 22.57] // Northeast coordinates
 ];
 
 const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/dpang311/ck21xo59h0cht1cmm2wjrmoki",
-    center: [114.18, 22.285],
-    zoom: 10,
-    minZoom: 10,
-    maxZoom: 14.5,
+    center: [114.147, 22.325],
+    zoom: 10.7,
     maxBounds: bounds
 });
 
 map.on("load", () => {
-    // define layer names
-    const layers = ["Asia", "Europe", "UK", "US"];
-    const colors = ["#ff7eb6", "#33b1ff", "#B384B9", "#6fdc8c"];
+    // Add Asia datasource
 
-    // create legend
-    const legend = document.getElementById("legend");
-
-    layers.forEach((layer, i) => {
-        const color = colors[i];
-        const item = document.createElement("div");
-        const key = document.createElement("span");
-        key.className = "legend-key";
-        key.style.backgroundColor = color;
-
-        const value = document.createElement("span");
-        value.innerHTML = `${layer}`;
-        item.appendChild(key);
-        item.appendChild(value);
-        legend.appendChild(item);
-    });
-
-    // Add asia
     map.addSource("asia", {
         type: "geojson",
-        data:
-            "https://raw.githubusercontent.com/Pangdi311/hkstreets/main/asia.geojson"
+        data: "https://raw.githubusercontent.com/hkstreets/maps/2408e9416895c92b57f00a2e6b490bf20689f121/asia.geojson",
+        generateId: true // This ensures that all features have unique IDs
     });
 
+    // Add Asia as a layer and style it
     map.addLayer({
-        source: "asia",
         id: "asia-layer",
         type: "circle",
+        source: "asia",
         paint: {
-            "circle-color": "#ff7eb6",
-            "circle-radius": 3,
-            "circle-stroke-width": 1,
-            "circle-stroke-color": "#ffffff",
-            "circle-opacity": 0.8,
-            "circle-stroke-opacity": 0.4
+            // The feature-state dependent circle-radius expression will render
+            // the radius size according to its magnitude when
+            // a feature's hover state is set to true
+            "circle-stroke-color": "#1C2B36",
+            "circle-opacity": [
+                "case",
+                ["boolean", ["feature-state", "hover"], false],
+                1,
+                0.65
+            ],
+            "circle-radius": [
+                "case",
+                ["boolean", ["feature-state", "hover"], false],
+                6,
+                4
+            ],
+            "circle-stroke-width": 0.5,
+            // The feature-state dependent circle-color expression will render
+            // the color according to its magnitude when
+            // a feature's hover state is set to true
+            "circle-color": [
+                "case",
+                ["boolean", ["feature-state", "hover"], false],
+
+                "#fff",
+                "#ff7eb6"
+            ]
         }
     });
 
-    // Add europe
-    map.addSource("europe", {
-        type: "geojson",
-        data:
-            "https://raw.githubusercontent.com/Pangdi311/hkstreets/main/europe.geojson"
-    });
-
-    map.addLayer({
-        source: "europe",
-        id: "europe-layer",
-        type: "circle",
-        paint: {
-            "circle-color": "#33b1ff",
-            "circle-radius": 3,
-            "circle-stroke-width": 1,
-            "circle-stroke-color": "#ffffff",
-            "circle-opacity": 0.8,
-            "circle-stroke-opacity": 0.4
-        }
-    });
-
-    // Add uk
-    map.addSource("uk", {
-        type: "geojson",
-        data:
-            "https://raw.githubusercontent.com/Pangdi311/hkstreets/main/uk.geojson"
-    });
-
-    map.addLayer({
-        source: "uk",
-        id: "uk-layer",
-        type: "circle",
-        paint: {
-            "circle-color": "#8a3ffc",
-            "circle-radius": 3,
-            "circle-stroke-width": 1,
-            "circle-stroke-color": "#ffffff",
-            "circle-opacity": 0.8,
-            "circle-stroke-opacity": 0.4
-        }
-    });
-
-    // Add us
-    map.addSource("us", {
-        type: "geojson",
-        data:
-            "https://raw.githubusercontent.com/Pangdi311/hkstreets/main/us.geojson"
-    });
-
-    map.addLayer({
-        source: "us",
-        id: "us-layer",
-        type: "circle",
-        paint: {
-            "circle-color": "#6fdc8c",
-            "circle-radius": 3,
-            "circle-stroke-width": 1,
-            "circle-stroke-color": "#ffffff",
-            "circle-opacity": 0.8,
-            "circle-stroke-opacity": 0.4
-        }
-    });
-    //add labels for asia
-    const popupmix = new mapboxgl.Popup({
+    const popupAsia = new mapboxgl.Popup({
         closeButton: false,
         closeOnClick: false,
         className: "popup"
     });
 
-    map.on("mouseenter", "asia-layer", (e) => {
-        // Change the cursor style as a UI indicator.
+    let hoverStateID = null;
+
+    map.on("mousemove", "asia-layer", (event) => {
         map.getCanvas().style.cursor = "pointer";
 
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popupmix
-            .setLngLat(e.lngLat)
+        if (event.features.length > 0) {
+            if (hoverStateID) {
+                map.removeFeatureState({
+                    source: "asia",
+                    id: hoverStateID
+                });
+            }
+
+            hoverStateID = event.features[0].id;
+
+            map.setFeatureState(
+                {
+                    source: "asia",
+                    id: hoverStateID
+                },
+                {
+                    hover: true
+                }
+            );
+        }
+
+        popupAsia
+            .setLngLat(event.lngLat)
             .setHTML(
-                e.features[0].properties.title +
-                e.features[0].properties.description +
-                e.features[0].properties.imagelink
+                event.features[0].properties.title +
+                event.features[0].properties.description +
+                event.features[0].properties.imagelink
             )
             .addTo(map);
     });
 
     map.on("mouseleave", "asia-layer", () => {
+        popupAsia.remove();
+        if (hoverStateID) {
+            map.setFeatureState(
+                {
+                    source: "asia",
+                    id: hoverStateID
+                },
+                {
+                    hover: false
+                }
+            );
+        }
+        hoverStateID = null;
+        // Reset the cursor style
         map.getCanvas().style.cursor = "";
-        popupmix.remove();
     });
 
-    //add labels for europe
-    const popupchi = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-        className: "popup"
-    });
 
-    map.on("mouseenter", "europe-layer", (e) => {
-        // Change the cursor style as a UI indicator.
-        map.getCanvas().style.cursor = "pointer";
-
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popupchi
-            .setLngLat(e.lngLat)
-            .setHTML(
-                e.features[0].properties.title +
-                e.features[0].properties.description +
-                e.features[0].properties.imagelink
-            )
-            .addTo(map);
-    });
-
-    map.on("mouseleave", "europe-layer", () => {
-        map.getCanvas().style.cursor = "";
-        popupchi.remove();
-    });
-
-    //add labels for meaning
-    const popupeng = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-        className: "popup"
-    });
-
-    map.on("mouseenter", "uk-layer", (e) => {
-        // Change the cursor style as a UI indicator.
-        map.getCanvas().style.cursor = "pointer";
-
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popupeng
-            .setLngLat(e.lngLat)
-            .setHTML(
-                e.features[0].properties.title +
-                e.features[0].properties.description +
-                e.features[0].properties.imagelink
-            )
-            .addTo(map);
-    });
-
-    map.on("mouseleave", "uk-layer", () => {
-        map.getCanvas().style.cursor = "";
-        popupeng.remove();
-    });
-
-    const popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-        className: "popup"
-    });
-
-    map.on("mouseenter", "us-layer", (e) => {
-        // Change the cursor style as a UI indicator.
-        map.getCanvas().style.cursor = "pointer";
-
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popup
-            .setLngLat(e.lngLat)
-            .setHTML(
-                e.features[0].properties.title +
-                e.features[0].properties.description +
-                e.features[0].properties.imagelink
-            )
-            .addTo(map);
-    });
-
-    map.on("mouseleave", "us-layer", () => {
-        map.getCanvas().style.cursor = "";
-        popup.remove();
-    });
+    //end
 });
+
+
