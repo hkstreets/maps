f98526a465a7281ef0384cfe8cec23a53173e2e7
Diana Pang <dianapanghoting@gmail.com>
2023-07-17 00:20:09 +0800

    updated popup header to h5

--- content/projects/asia-world-city/assets/script.js
+++ content/projects/asia-world-city/assets/script.js
@@ -176,7 +176,7 @@ map.on("load", () => {
         // Populate the popup and set its coordinates
         // based on the feature found.
         popup.setLngLat(e.lngLat).setHTML(
-            "<h3 class='popupheader'>" + title + "</h3>" +
+            "<h5 class='popupheader'>" + title + "</h5>" +
             "<p>" + description + "</p>" +
             "<img src=" + imageUrl + ">"
         ).addTo(map);
