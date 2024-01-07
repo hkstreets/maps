5774f4400b5d820d5fc1713e6bb8bf16d94122b1
Mart van de Ven <m@type.hk>
2024-01-07 17:32:54 +0800

    Add boilerplate for "Trade and Commerce" project

---
+++ content/projects/trade-and-commerce/assets/script.js
@@ -0,0 +1,14 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiZHBhbmczMTEiLCJhIjoiY2tidmNld2cyMDA5djJwbXYzZjZsNTB0OSJ9.hvW536UZKn2wDDH6yDjJ2g";
+
+const bounds = [
+    [113.7, 22.18], // Southwest coordinates
+    [114.5, 22.57]  // Northeast coordinates
+];
+
+const map = new mapboxgl.Map({
+    container: "hkstreets-map",
+    style: "mapbox://styles/dpang311/ck21xo59h0cht1cmm2wjrmoki",
+    center: [114.18, 22.285],
+    zoom: 12,
+    maxBounds: bounds
+});
---
+++ content/projects/trade-and-commerce/index.md
@@ -0,0 +1,9 @@
+---
+title: "Trade and Commerce"
+date: 2024-01-07T17:31:32+08:00
+draft: true
+layout: map
+noBaseStyles: true
+---
+
+**A new mapping project.**
